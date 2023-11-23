09ad9ece693e12cf0a56f2a7bb1449c024347c49
Yordan Georgiev <yordan.georgiev@csitea.net>
2023-11-23 11:47:19 +0200

    add the do_list_secrets function

---
+++ src/bash/run/list-secrets.func.sh
@@ -0,0 +1,32 @@
+# !/bin/bash
+# ----------------------------------------------------------------------
+# Function to recursively list entries in a group (or subgroup)
+# ----------------------------------------------------------------------
+# Globals:
+#   DATABASE_PATH: Path to the KeePassXC database
+#   KEYFILE_PATH: Path to the KeePassXC keyfile
+#   KEEPASSXC_GROUP: Group to list entries from
+do_list_entries() {
+    local group_path=${1:-$KEEPASSXC_GROUP}
+    # List groups and entries under the current group path
+    keepassxc-cli ls --no-password ${DATABASE_PATH} "${group_path}" --key-file=${KEYFILE_PATH} | while read line; do
+        # Check if line is a group
+        if [[ $line == */ ]]; then
+            # Recursively list entries in this subgroup
+            new_group_path="${group_path}/${line}"
+            do_list_entries "${new_group_path%/}" # Remove trailing slash
+        else
+            # Print details of the entry
+            echo "Entry: ${group_path}/${line}"
+            # Uncomment the next lines if you want to show entry details
+            username=$(keepassxc-cli show --no-password --quiet ${DATABASE_PATH} "${group_path}/${line}" --key-file=${KEYFILE_PATH} --attributes=UserName)
+            echo keepassxc-cli show --no-password --quiet ${DATABASE_PATH} "${group_path}/${line}" --key-file=${KEYFILE_PATH} --attributes=Password \| xclip
+            #password=$(keepassxc-cli show --no-password --quiet ${DATABASE_PATH} "${group_path}/${line}" --key-file=${KEYFILE_PATH} --attributes=Password)
+            echo "Username: $username"
+            #echo "Password: $password"
+            echo ""
+        fi
+    done
+
+    export exit_code="0"
+}
