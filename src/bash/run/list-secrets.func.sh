7d84736434bc6e10766c76c3b3942c4ee6a329f4
Yordan Georgiev <yordan.georgiev@csitea.net>
2023-11-23 17:38:10 +0200

    fix the clip bug for not selecting the txt into the clipboard

--- src/bash/run/list-secrets.func.sh
+++ src/bash/run/list-secrets.func.sh
@@ -6,7 +6,7 @@
 #   DATABASE_PATH: Path to the KeePassXC database
 #   KEYFILE_PATH: Path to the KeePassXC keyfile
 #   KEEPASSXC_GROUP: Group to list entries from
-do_list_entries() {
+do_list_secrets() {
     local group_path=${1:-$KEEPASSXC_GROUP}
     # List groups and entries under the current group path
     keepassxc-cli ls --no-password ${DATABASE_PATH} "${group_path}" --key-file=${KEYFILE_PATH} | while read line; do
@@ -14,13 +14,13 @@ do_list_entries() {
         if [[ $line == */ ]]; then
             # Recursively list entries in this subgroup
             new_group_path="${group_path}/${line}"
-            do_list_entries "${new_group_path%/}" # Remove trailing slash
+            do_list_secrets "${new_group_path%/}" # Remove trailing slash
         else
             # Print details of the entry
             echo "Entry: ${group_path}/${line}"
             # Uncomment the next lines if you want to show entry details
             username=$(keepassxc-cli show --no-password --quiet ${DATABASE_PATH} "${group_path}/${line}" --key-file=${KEYFILE_PATH} --attributes=UserName)
-            echo keepassxc-cli show --no-password --quiet ${DATABASE_PATH} "${group_path}/${line}" --key-file=${KEYFILE_PATH} --attributes=Password \| xclip
+            echo keepassxc-cli show --no-password --quiet ${DATABASE_PATH} "${group_path}/${line}" --key-file=${KEYFILE_PATH} --attributes=Password \| xclip -selection clipboard
             #password=$(keepassxc-cli show --no-password --quiet ${DATABASE_PATH} "${group_path}/${line}" --key-file=${KEYFILE_PATH} --attributes=Password)
             echo "Username: $username"
             #echo "Password: $password"
